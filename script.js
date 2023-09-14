d11207556d2a5e50a1f6906cc03145dfc8361c8a
GoriNeoRage <119440148+GoriNeoRage@users.noreply.github.com>
2023-09-14 18:08:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-for (let i = 1; i < 151; i++) {
+for (let i = 1; i < 100; i++) {
     const pokemon = document.createElement('div');
     const label = document.createElement('span');
     pokemon.classList.add('pokemon');
@@ -12,4 +12,19 @@ for (let i = 1; i < 151; i++) {
     pokemon.appendChild(newImg);
     pokemon.appendChild(label);
     container.appendChild(pokemon);
+}
+
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+button.addEventListener('click', function () {
+    const newColor = makeRandomColor();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
 }
